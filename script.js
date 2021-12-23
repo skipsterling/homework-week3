98ae9474e09e0922b35f856d66070d78451c6ffc
Thomas James <tomsterlingjames@outlook.com.au>
2021-12-23 10:40:51 +1000

    variables added to js file.

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 
+var generateBtn = document.querySelector("#generate");
+var charNum = "1234567890";
+var charLow = "abcdefghijklmnopqrstuvwxyz" ;
+var charUp = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var charSpecial = "~`!@#$%^&*()_-+={[}]|\:;'<,>.?/";
+var passwordLength;
+var numInspect;
+var specialInspect;
+var upInspect;
+
+
